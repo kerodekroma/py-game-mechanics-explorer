0d13d63a353c4b76449488182cddbb0a283c3d3c
KeroDeKroma <kerodekroma@gmail.com>
2024-11-10 00:08:36 -0500

    feat: basic walking

--- mechanics/00_walking/__init__.py
+++ mechanics/00_walking/__init__.py
@@ -5,7 +5,7 @@ import pygame
 
 import sys
 
-from toolbox import colors
+from toolbox import colors, font
 
 # Initializing Pygame
 pygame.init()
@@ -15,11 +15,15 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
 palette = colors.PALETTE
+pixel_font = font.pixel_font()
 
-# Defining a color to print in all the screen
+# Defining the color to print in the screen
 BG_COLOR = palette[0]
-
 FLOOR_COLOR = palette[13]
+
+# 
+GRAVITY = 200
+SPEED = 500
 
 # The screen is almost ready, this is just the definition
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -29,10 +33,13 @@ pygame.display.set_caption("mechanics > walking")
 # clock
 clock = pygame.time.Clock()
 
-# setup
+# setup dude
 dude = pygame.image.load('./assets/img/orangito32x32.png').convert_alpha()
+dude_x = 100
+dude_y = 250
+dude_vel_x = 0
 
-
+# setup floor
 tile_floor = pygame.image.load('./assets/img/bg_one.png').convert_alpha()
 tile_floor_width, tile_floor_height = tile_floor.get_size()
 floor_surface = pygame.Surface((screen.get_width(), tile_floor_height ), pygame.SRCALPHA)
@@ -40,8 +47,28 @@ floor_surface = pygame.Surface((screen.get_width(), tile_floor_height ), pygame.
 for x in range(0, screen.get_width(), tile_floor_width):
     floor_surface.blit(tile_floor, (x, 0))
 
+# adding the defs to check if the input is to the left/right
+def is_button_down(event):
+    return event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.FINGERDOWN
+
+def is_button_up(event):
+    return event.type == pygame.MOUSEBUTTONUP or event.type == pygame.FINGERUP
+
+def is_input_left_active(event, screen):
+    mouse_pos = pygame.mouse.get_pos()
+    if is_button_down(event):
+        return  mouse_pos[0] < screen.get_width() / 4
+    return event.type == pygame.KEYDOWN and (event.key == pygame.K_a or event.key == pygame.K_LEFT)
+        
+def is_input_right_active(event, screen):
+    mouse_pos = pygame.mouse.get_pos()
+    if is_button_down(event):
+        return mouse_pos[0] > ( screen.get_width() / 4 ) * 3
+    return event.type == pygame.KEYDOWN and (event.key == pygame.K_d or event.key == pygame.K_RIGHT)
+
 # Flag to run our loop, it's optional in this example but useful to get used too
 while True:
+    dt = clock.tick(60) / 1000
     # In this for loop is crazy! because its always listening which event is triggering
     for event in pygame.event.get():
         # And once is equal when a user closes the window
@@ -50,12 +77,32 @@ while True:
             pygame.quit()
             sys.exit()
 
-    dt = clock.tick(60) / 1000
+        if is_input_left_active(event, screen):
+            dude_vel_x -= SPEED * dt 
+
+        if is_input_right_active(event, screen):
+            dude_vel_x += SPEED * dt 
+            
+        if event.type == pygame.KEYUP or is_button_up(event):
+            dude_vel_x = 0
+
 	# This is the magic sentence where our screen finally renders our DEMO_BG_COLOR, yay!
     screen.fill(BG_COLOR)
 
     # printing the dude image
-    screen.blit(dude, (100, 100))
+    screen.blit(dude, (dude_x, dude_y))
+
+    dude_x += dude_vel_x
+
+    # detect bounds and limiting the dude in the screen
+    if(dude_y + dude.get_rect().height <= 500):
+        dude_y += GRAVITY * dt
+
+    if(dude_x <= 0):
+        dude_x = 0
+
+    if(dude_x + dude.get_rect().width >= screen.get_width()):
+        dude_x = screen.get_width() - dude.get_rect().width
 
     # floor edge
     screen.blit(floor_surface, (0, 500))
@@ -64,8 +111,15 @@ while True:
     floor_bg_rect = pygame.Rect(0, 505, screen.get_width(), 100)
     pygame.draw.rect(screen, FLOOR_COLOR, floor_bg_rect)
 
+    # FPS
+    fps = clock.get_fps()
+    text_content = f"FPS: {int(fps)}"
+    text_surface = pixel_font.render(text_content, True, palette[2])
+    text_rect = text_surface.get_rect(center=(100, 100))
+    screen.blit(text_surface, text_rect)
+
 	# This method refreshes all the screen, is part of a good practice keep it here
     pygame.display.flip()
 
     #clock
-    pygame.time.Clock().tick(60)
+    clock.tick(60) # Limit to 60 FPS
---
+++ toolbox/font.py
@@ -0,0 +1,5 @@
+import pygame
+
+def pixel_font(size=32):
+    font = pygame.font.Font('./assets/font/PixelSimpel.otf', size)
+    return font
