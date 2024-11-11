5d1cf6cf1ceafead47ca0be004ad243b71b0ff2a
KeroDeKroma <kerodekroma@gmail.com>
2024-11-11 00:41:59 -0500

    feat: friction and drag

--- mechanics/03_friction/__init__.py
+++ mechanics/03_friction/__init__.py
@@ -24,8 +24,8 @@ FLOOR_COLOR = palette[13]
 # 
 GRAVITY = 200
 MAX_SPEED = 500
-ACCELERATION = 1500
-DRAG = 400
+ACCELERATION = 50
+DRAG = 0.9
 
 # The screen is almost ready, this is just the definition
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -69,18 +69,6 @@ def is_input_right_active(event, screen):
         return mouse_pos[0] > ( screen.get_width() / 4 ) * 3
     return event.type == pygame.KEYDOWN and (event.key == pygame.K_d or event.key == pygame.K_RIGHT)
 
-# drag effect
-def apply_drag(velocity, drag, dt):
-    if velocity > 0:
-        velocity -= drag * dt
-        if velocity < 0:
-            velocity = 0
-    if velocity < 0:
-        velocity += drag * dt
-        if velocity > 0:
-            velocity = 0
-    return velocity
-
 # Flag to run our loop, it's optional in this example but useful to get used too
 while True:
     dt = clock.tick(60) / 1000
@@ -108,12 +96,8 @@ while True:
     screen.blit(dude, (dude_x, dude_y))
 
     dude_vel_x += dude_acc_x * dt
-    dude_vel_x = max(-MAX_SPEED, min(MAX_SPEED, dude_vel_x))
-
-    if dude_acc_x == 0:
-        dude_vel_x = apply_drag(dude_vel_x, DRAG, dt)
-
-    dude_x += dude_vel_x * dt
+    dude_vel_x *= DRAG
+    dude_x += dude_vel_x
 
     # detect bounds and limiting the dude in the screen
     if(dude_y + dude.get_rect().height <= 500):
@@ -141,14 +125,14 @@ while True:
 
     # Debugging
     # Velocity
-    text_content = f"Velocity: { dude_vel_x }"
+    text_content = f"Velocity: {int( dude_vel_x )}"
     text_surface = pixel_font.render(text_content, True, palette[2])
-    text_rect = text_surface.get_rect(topleft=(50, 130))
+    text_rect = text_surface.get_rect(center=(100, 130))
     screen.blit(text_surface, text_rect)
     # Acceleration
     text_content = f"Acceleration: {int( dude_acc_x )}"
     text_surface = pixel_font.render(text_content, True, palette[2])
-    text_rect = text_surface.get_rect(topleft=(50, 160))
+    text_rect = text_surface.get_rect(center=(100, 160))
     screen.blit(text_surface, text_rect)
 
 	# This method refreshes all the screen, is part of a good practice keep it here
